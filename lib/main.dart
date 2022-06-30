bb0076ba02185a04d2e5ca4cfff2774d2e045e91
alamin-karno <amin15-1951@diu.edu.bd>
2022-06-30 09:39:13 +0600

    e-Commerce App - Flutter Bloc package update version - 8.0.1

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_ecommerce_app/simple_bloc_observer.dart';
 import 'repositories/repositories.dart';
 import 'bloc/blocs.dart';
 import 'config/config.dart';
@@ -9,7 +10,12 @@ import 'screens/screens.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(const MyApp());
+  BlocOverrides.runZoned(
+    () {
+      runApp(const MyApp());
+    },
+    blocObserver: SimpleBlocObserver(),
+  );
 }
 
 class MyApp extends StatelessWidget {
